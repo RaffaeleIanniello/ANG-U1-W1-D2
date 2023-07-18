fd66c4a92057efe1646240f5a8bc1c28b8824de3
Raffaele <devout.official@gmail.com>
2023-07-18 15:16:26 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+"use strict";
+class BankAccount {
+    constructor(balanceInit = 0) {
+        this.balance = balanceInit;
+    }
+    deposit(quant) {
+        this.balance += quant;
+    }
+    withdraw(quant) {
+        if (this.balance >= quant) {
+            this.balance -= quant;
+        }
+        else {
+            console.log("Fondi non suff.");
+        }
+    }
+    getBalance() {
+        return this.balance;
+    }
+}
+class SonAccount extends BankAccount {
+    // Inherits properties and methods from BankAccount
+    constructor(balanceInit = 0) {
+        super(balanceInit);
+    }
+}
+class MotherAccount extends BankAccount {
+    constructor(balanceInit = 0) {
+        super(balanceInit);
+    }
+    addInterest() {
+        const interest = this.balance * 0.1;
+        this.balance += interest;
+    }
+}
+// Creazione dei conti correnti
+const sonAccount = new SonAccount();
+const motherAccount = new MotherAccount();
+// Operazioni sul conto del figlio
+sonAccount.deposit(100);
+sonAccount.withdraw(50);
+// Operazioni sul conto della madre
+motherAccount.deposit(500);
+motherAccount.withdraw(200);
+motherAccount.addInterest();
+// Stampa dei saldi attuali
+console.log("Saldo conto figlio: ", sonAccount.getBalance());
+console.log("Saldo conto madre: ", motherAccount.getBalance());
